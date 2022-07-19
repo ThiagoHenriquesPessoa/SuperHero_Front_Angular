84d166a5c055aff1f0c29706c330ccbafccd70ad
Thiago Pessoa <thiagokxl5@gmail.com>
2022-07-18 22:45:04 -0300

    Preparando os crud

--- src/app/services/super-hero.service.ts
+++ src/app/services/super-hero.service.ts
@@ -13,7 +13,23 @@ export class SuperHeroService {
   constructor(private http: HttpClient) { }
 
   public getSuperHeros(): Observable<SuperHero[]> {   
-
     return this.http.get<SuperHero[]>(`${environment.apiUrl}/${this.url}`);
+  }
+
+  public updateHero(hero: SuperHero): Observable<SuperHero[]> {   
+    return this.http.put<SuperHero[]>(
+      `${environment.apiUrl}/${this.url}`, 
+      hero);
+  }
+
+  public createHero(hero: SuperHero): Observable<SuperHero[]> {   
+    return this.http.post<SuperHero[]>(
+      `${environment.apiUrl}/${this.url}`, 
+      hero);
+  }
+
+  public deleteHero(hero: SuperHero): Observable<SuperHero[]> {   
+    return this.http.delete<SuperHero[]>(
+      `${environment.apiUrl}/${this.url}/${hero.id}`);
   }
 }
